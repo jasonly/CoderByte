e4a23c54a16fe232da135aa9da0bd857e6a03aa9
Jason Ly <jasonly@Jasons-MacBook-Air.local>
2014-07-03 22:08:04 -0700

    CoderByte longestWord Solutio

---
+++ 03-CoderByte-Longest-Word.js
@@ -0,0 +1,21 @@
+//Longest Word
+function longestWord(sen) {
+	var sentenceSplit = sen.split(' ');
+	var greatestWord = '';
+
+	for(var i = 0; i < sentenceSplit.length; i++){
+		var word = sentenceSplit[i];
+
+		if (word.length > greatestWord.length){
+			greatestWord = word;
+		}
+	}
+	return greatestWord;
+};
+
+//The longestWord function takes the longest word from a sentence and returns it
+//sentenceSplit splits the sentence into words
+//greatestWord is an empty variable where we'll store the longest word
+//Used a for loop to go through my sentence word by word and stored each word in a new variable called word
+//The if statement compares the current word to the greatestWord so far, and if it's greater then it
+//replaces that word.
