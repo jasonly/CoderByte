73ecdd7665cbc23e3093ef097fc3183c2819fd40
Jason Ly <jasonly@Jasons-MacBook-Air.local>
2014-07-03 21:53:47 -0700

    First commit firstReverse solution

---
+++ 01-CoderByte-First-Reverse.js
@@ -0,0 +1,8 @@
+//First Reverse
+function firstReverse(str) {
+	return str.split('').reverse().join('')
+};
+
+//split('') breaks up the str argument into individual characters
+//reverse() reverses the string
+//join('') joins these reversed characters
