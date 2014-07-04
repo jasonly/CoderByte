4269e0b16c635a5f0f22b31d8fd69aa1ce9fd880
Jason Ly <jasonly@Jasons-MacBook-Air.local>
2014-07-04 01:03:51 -0700

    CoderByte letterCapitalize Solutio  Please enter the commit message for your changes. Lines starting

---
+++ 06-CoderByte-Letter-Capitalize.js
@@ -0,0 +1,20 @@
+//Letter Capitalize
+//Using the JavaScript language, have the function LetterCapitalize(str) take the str parameter being passed 
+//and capitalize the first letter of each word. Words will be separated by only one space.
+function letterCapitalize (str) {
+	var splitToWords = str.split(' ');
+		//if str = the cat is here then --> [the, cat, is, here]
+	for (var i = 0; i < splitToWords.length; i++){
+		var splitToLetters = splitToWords[i].split('');
+		//[the...] --> [t, h, e] this cycles through then goes on to the next word
+		splitToLetters[0] = splitToLetters[0].toUpperCase();
+		splitToWords[i] = splitToLetters.join('');
+		//i = the at the moment and it gets joined back into a word
+	}
+	return splitToWords.join(' ');
+};
+
+//Take a string then split it into words. Then loop through that array of words and split each word
+//into an array of letters. The first element of that array is then capitalized with toUpperCase
+//Then the word that was split into an array of letters is joined back and we return
+//the joined array of split words 
