9da070b7c5e504f08e89d5562b0e7b63e672b185
Jason Ly <jasonly@Jasons-MacBook-Air.local>
2014-07-03 22:04:45 -0700

    CoderByte firstFactorial Solution

---
+++ 02-CoderByte-First-Factorial.js
@@ -0,0 +1,13 @@
+//First Factorial
+function FirstFactorial(num) {
+  if (num == 0) {
+      return 1;
+  }else if (num > 1){
+    return num * FirstFactorial(num – 1);
+  }
+};
+
+//If num is equal to 0 then return 1, if num is greater than 1 then return num * num-1
+//based this one off of the proof for factorials.
+//the else if num = 3 then 3*(3-1) -> (2-1) -> ends at if num == 0 which is 1
+//Is this like inception?
