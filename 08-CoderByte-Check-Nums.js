0ee7a4e8a656148c07518c345b31eaed04fc32cc
Jason Ly <jasonly@Jasons-MacBook-Air.local>
2014-07-04 01:10:30 -0700

    CoderByte checkNums Solution

---
+++ 08-CoderByte-Check-Nums.js
@@ -0,0 +1,14 @@
+//Check Nums
+//Using the JavaScript language, have the function CheckNums(num1,num2) take both parameters being passed and 
+//return the string true if num2 is greater than num1, otherwise return the string false. If the parameter 
+//values are equal to each other then return the string -1. 
+function checkNums (num1, num2){
+	if (num2 > num1) {
+		return num2 > num1;
+	} else {
+		return "-1";
+	}
+};
+
+//Compares two arguments using an if statement. If num2 is greater than num1 return true, for all
+//other instances return -1.
