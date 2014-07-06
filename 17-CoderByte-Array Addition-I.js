ae04d254519b6db0c6495c26cf9dfe053ecd3697
Jason Ly <jason.ly@gmail.com>
2014-07-05 17:33:45 -0700

    CoderByte Array Addition Solution

---
+++ 17-CoderByte-Array Addition-I.js
@@ -0,0 +1,29 @@
+//Array Addition I
+//Using the JavaScript language, have the function ArrayAdditionI(arr) take the array of numbers stored in arr
+//and return the string true if any combination of numbers in the array can be added up to equal the largest
+//number in the array, otherwise return the string false. For example: if arr contains [4, 6, 23, 10, 1, 3] the
+//output should return true because 4 + 6 + 10 + 3 = 23. The array will not be empty, will not contain all the 
+//same elements, and may contain negative numbers. 
+
+function ArrayAdditionI(arr) {
+	var arrSort = arr.sort(function(a,b){return a-b});
+		//sorts all numbers in the array from smallest to largest
+	var popSort = arrSort.pop();
+		//pops out the last number, realized that there could possibly be duplicate numbers
+	
+	var arrNotGreatest = function(arr) {
+		return arr != popSort;
+	}
+
+	var filter = arr.filter(arrNotGreatest);
+		//created an array that does not contain all occurrences of the largest number
+		//and named this array under the variable filter
+    
+    var sumArr = filter.reduce(function(a,b){return a+b});
+    	//added up all the elements in this array
+        
+    return sumArr == popSort;
+    	//compared the sum to the highest number
+};
+
+
